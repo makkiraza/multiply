066fbe3723234edb8adafbefc794acb02cfaeb3a
makkiraza <makkirazaa@gmail.com>
2023-09-28 03:33:24 +0500

    ddoone

---
+++ app.js
@@ -0,0 +1,9 @@
+var a = prompt ('enter your first value')
+var b = prompt ('enter your second value')
+var c = prompt ('enter your third value')
+
+function mul (a,b,c){
+    return a*b*c
+}
+
+document.write(mul(a,b,c));
